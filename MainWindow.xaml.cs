12a47ae36c3319da2cbc4b8a8092a5a2db5e8737
LaminarDynamics <LaminarDynamics@gmail.com>
2022-07-21 23:49:22 -0800

    Added Comments and Changed Zoom/Translation logic

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,9 +20,7 @@ namespace Counter_Wpf
     /// </summary>
     public partial class MainWindow : Window
     {
-
-        public double mouse_scroll = 0;
-        public double image_zoom = 1;
+        private double image_zoom = 1;
 
         private Point start;   
         private Point translate_amount;
@@ -31,31 +29,30 @@ namespace Counter_Wpf
         public MainWindow()
         {
             InitializeComponent();
-
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-
-
+            // Get screen size to dynamically size components
             double screen_width = SystemParameters.PrimaryScreenWidth;
             double screen_height = SystemParameters.PrimaryScreenHeight;
-            //MessageBox.Show(screen_width.ToString());
-
+      
+            // Scale of main photo
             double image_scale_width = .75;
-
             image.Width = screen_width * image_scale_width;
 
+            // Sidebar scaling
             listBox.Height = screen_height * .95;
             listBox.Width = (screen_width - image.Width) * .94;
 
+            // Slider scaling
             slider.Height = screen_height * .95;
             slider.Margin = new Thickness(image.Width - listBox.Width, 0, 0, 0);
-
         }
 
         private void image_MouseWheel(object sender, MouseWheelEventArgs e)
         {
+            // Zoom controls
             double zoom_speed = .5;
             if (e.Delta > 0) // Zoom in 
             {
@@ -65,79 +62,58 @@ namespace Counter_Wpf
             {
                 image_zoom -= zoom_speed;
             }
-
-            ChangeZoom(image_zoom);
-            //Point mouse_pos = Mouse.GetPosition(image);
+            //ChangeZoom(image_zoom);
+            ChangeTranslationAndZoom();
         }
 
-        private void ChangeZoom(double new_zoom)
+        private void ChangeTranslationAndZoom()
         {
-            image.RenderTransform = new ScaleTransform(new_zoom, new_zoom);
-            slider.Value = new_zoom;
-            textBox.Text = image_zoom.ToString();
-
+            // Move image by translation amount
+            Matrix X = new Matrix();
+            X.Translate(translate_amount.X, translate_amount.Y);
+            X.Scale(image_zoom, image_zoom);
+            image.RenderTransform = new MatrixTransform(X);
         }
 
         private void slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
+            // Zoom slider
             image_zoom = slider.Value;
-            ChangeZoom(image_zoom);
+            ChangeTranslationAndZoom();
         }
-
 
         private void image_MouseMove_1(object sender, MouseEventArgs e)
         {
+            // Used with left click to slide image
             mouse_pos = e.MouseDevice.GetPosition(image);
             textBox.Text = mouse_pos.ToString();
 
-            if (e.LeftButton == MouseButtonState.Pressed)
+            if (e.LeftButton == MouseButtonState.Pressed) // If moving while clicked
             {
+                // Get mouse move distance
                 double x_distance = start.X - mouse_pos.X;
                 double y_distance = start.Y - mouse_pos.Y;
 
+                // Append movement to translation amount
                 translate_amount.X += x_distance * -.5;
                 translate_amount.Y += y_distance * -.5;
                 textBox.Text = translate_amount.X.ToString() + "\n" + translate_amount.Y.ToString();
 
-
-                Matrix X = new Matrix();
-                X.Translate(translate_amount.X, translate_amount.Y);
-                X.Scale(image_zoom, image_zoom);
-
-
-                image.RenderTransform = new MatrixTransform(X);
+                // Move image by translation amount
+                ChangeTranslationAndZoom();
             }
         }
 
         private void image_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
+            // Important to get mouse start position for image translation to work
             start = e.MouseDevice.GetPosition(image);
             textBox.Text = "Start pos is: " + start.ToString();
         }
 
-
-
         private void button_Click(object sender, RoutedEventArgs e)
         {
-            //translate_amount.X -= 10;
-            //translate_amount.Y -= 10;
 
-            //Matrix X = new Matrix();
-            //X.Translate(translate_amount.X, translate_amount.Y);
-            //X.Scale(image_zoom, image_zoom);
-            //image.RenderTransform = new MatrixTransform(X);
-        }
-
-        private void image_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
-        {
-            //start.X = 0;
-            //start.Y = 0;
-
-            //translate_amount.X = 0;
-            //translate_amount.Y = 0;
         }
     }
-
-
-
 }
